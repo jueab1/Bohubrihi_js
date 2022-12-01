12474feac475942db5a12ab6e8dc0110658839a7
jueab1 <hoque273@gmail.com>
2022-12-01 08:00:27 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*console.log("i am learning javascript");
+console.log("i love dsa");
+document.write("js file is connected")*/
+
+/*var x;
+x= prompt("Enter your Name:")
+console.log(x);
+document.write(x);*/ 
+
+// Increment Operator a++ / ++a 
+
+/*var a= 5;
+var b= 8;
+// a++
+// b++
+++a
+++b
+console.log(a);
+console.log(b);*/
+
+var temp = prompt("Please Enter a Temparature: ")
+var result = 9/5*temp + 32
+alert("Farenheit: "+ result+ "Degree")
+console.log("Task complete");
